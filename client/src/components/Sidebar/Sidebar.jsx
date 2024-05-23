d07cf7818b99167d1cea1fe542930f21038b19d7
Christian Davis <cdavis20@stanford.edu>
2024-05-23 14:23:28 -0700

    Make sidebar clickable

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,62 +1,32 @@
-import React from "react";
-import { SiFeedly } from "react-icons/si";
-import { BiSolidVideos } from "react-icons/bi";
-import { MdGroups, MdBook, MdEmojiPeople, MdScience } from "react-icons/md";
+import React, { useState } from "react";
+import { POST_TYPES } from "../../constants/constants";
 
-import {
-  IoChatboxEllipsesSharp,
-  IoBookmarks,
-  IoBriefcase,
-} from "react-icons/io5";
-import { BsFillQuestionSquareFill } from "react-icons/bs";
-import { FaUserGraduate, FaCalendarDay } from "react-icons/fa";
-import testImage from "../../assets/profilepic.jpg";
-import { Friends } from "../../data/dummyData";
-import FriendsList from "../FriendsList/FriendsList";
+const Sidebar = ({ onFilterChange }) => {
+  const [activeCategory, setActiveCategory] = useState(POST_TYPES[0]);
 
-const Sidebar = () => {
+  const handleCategoryClick = (category) => {
+    setActiveCategory(category);
+    onFilterChange(category);
+  };
+
   return (
-    <div
-      style={{ flex: 3, height: "calc(100vh - 50px)" }}
-      className="custom-scrollbar overflow-y-auto sticky top-[50px]"
-    >
-      <div className=" p-[20px] ">
+    <div style={{ flex: 3 }}>
+      <div className="p-[20px]">
         <ul className="sidebarList m-0 p-0">
-          <li>
-            <MdBook />
-            <span>Classes</span>
-          </li>
-          <li>
-            <MdEmojiPeople />
-            <span>Clubs</span>
-          </li>
-          <li>
-            <MdScience />
-            <span>Research</span>
-          </li>
-          <li>
-            <MdGroups />
-            <span>Hobbies</span>
-          </li>
-          <li>
-            <IoBriefcase />
-            <span>Work</span>
-          </li>
+          {POST_TYPES.map((category) => (
+            <li
+              key={category}
+              onClick={() => handleCategoryClick(category)}
+              className={`cursor-pointer p-[10px] mb-[10px] ${
+                activeCategory === category
+                  ? "bg-blue-500 text-white"
+                  : "hover:bg-gray-200"
+              }`}
+            >
+              <span>{category}</span>
+            </li>
+          ))}
         </ul>
-        <div className="button">
-          <button className="rounded-md bg-slate-200  w-[150px] p-[10px]">
-            {" "}
-            See More
-          </button>
-        </div>
-        <hr className="mt-[20px]" />
-        {/* <div className="mt-[20px]">
-          <ul className="sidebarList">
-            {Friends.map((friend) => (
-              <FriendsList key={friend.id} friend={friend} />
-            ))}
-          </ul>
-        </div> */}
       </div>
     </div>
   );
---
+++ client/src/constants/constants.jsx
@@ -0,0 +1,1 @@
+export const POST_TYPES = ["Classes", "Clubs", "Research", "Hobbies", "Work"];
