c17b7ecb7da797b3b7dbdd1dc3787dbc77e4182c
Aman Ladia <24718566+amsee01@users.noreply.github.com>
2024-06-04 11:04:09 -0700

    Added z-index to avoid overflow on navbar.

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = ({ handleSearch }) => {
 
 
   return (
-    <div className="h-[50px] w-full bg-blue-600 flex items-center sticky top-0">
+    <div className="h-[50px] w-full bg-blue-600 flex items-center sticky top-0" style={{zIndex : 999}}>
       <div className="left bg-red" style={{ flex: 3 }}>
         <Link to={"/"}>
           <div className="logodiv cursor-pointer">
